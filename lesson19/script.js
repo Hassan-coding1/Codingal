adcebbf34da57b0e7a23da0a725cf0a00143c44b
Hassan.coding1 <manik.sarkar1@gmail.com>
2025-08-03 15:55:06 +0530

    Project

---
+++ lesson19/script.js
@@ -0,0 +1,30 @@
+console.log("Hi this is Hassan")
+
+//creating a variable of type number
+var a = 22
+
+//creating a variable of type string
+var b = "Hi My name is Hassan"
+
+//creating a variable of type boolean
+var c = true
+var d = false
+
+//performing operations
+var e = a*24+50-32
+var f = 10+26*21-21/2
+
+var g = 50
+g += 10
+g -= 20
+g *= 30
+g /= 40
+
+//printing
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(e)
+console.log(f)
+console.log(g)
