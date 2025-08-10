58019dd0648f32ffeac2ffd1c33fa4eced673395
Hassan.coding1 <manik.sarkar1@gmail.com>
2025-08-10 20:54:53 +0530

    hi

---
+++ lesson20/style.js
@@ -0,0 +1,35 @@
+function gotoWashroom () {
+    console.log("Ma'am ")
+    console.log("May I go to washroom")
+    console.log("Please let me go ma'am")
+}
+
+gotoWashroom()
+gotoWashroom()
+gotoWashroom()
+gotoWashroom()
+
+function add(a,b,c) {
+   var res=a+b+c
+   return res
+}
+
+var sum = add(22,32,42)
+console.log(sum)
+
+console.log(add(73,72,24))
+
+var add1 = (a,b,c) => a+b+c
+
+console.log(add1(6,4,7))
+
+var b = x => x*9
+
+console.log(b(100))
+
+
+
+
+
+
+
