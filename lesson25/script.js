9cd19281142b54aced9eccfb1c225c411100e0de
Hassan.coding1 <manik.sarkar1@gmail.com>
2025-08-30 15:55:45 +0530

    hi

---
+++ lesson25/script.js
@@ -0,0 +1,50 @@
+var a = 842
+var b = 'Hi, this is Hassan'
+var c = true
+var d = false
+var e = null
+var f = undefined
+
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(typeof(c))
+console.log(typeof(d))
+console.log(typeof(e))
+console.log(typeof(f))
+
+const x = 92
+console.log(typeof(x))
+console.log(z)
+
+let y = 542
+y = 325
+console.log(y)
+
+var z = 523
+z = 894
+console.log(z)
+
+const w = 'hi'
+//w = 'hello'
+//this will show error
+
+if(z<50){
+    console.log("It is less than 50")
+}
+
+if(z<100){
+    console.log("It is less than 100")
+}
+
+if(z<500){
+    console.log("It is less than 500")
+}
+
+else{
+    console.log("It is greater than 500")
+}
+
+let i=1
+for(i=0;i<=10;i++){
+    console.log("Hii")
+}
